510c6d8a2904fe43ce30c594474f32686d4a59b8
Johannes Langosch <johanneslangosch@MBPvonJohannes.fritz.box>
2021-02-04 19:52:25 +0100

    Kaptitel 61 lokales temporäres Speichern

--- App.js
+++ App.js
@@ -1,15 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import Quote from './JS/components/Quote';
+import NewQuote from './JS/components/NewQuote';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const data = [
+
+  { text: "Zitat 1 jhgjhghgjhgjhgjhghj", author: "Author1" },
+  { text: "Zitat 2 sdjhfkshsdfkjhsdfkj", author: "Author2" },
+  { text: "Zitat 3 skdjhfkjhshdfkjshkj", author: "Author3" },
+
+];
+
+export default class App extends Component {
+
+  state = { index: 0, showNewQuoteScreen: false, quotes: data };
+
+  _addQuote = (text, author) => {
+
+    let { quotes } = this.state;
+    if (text && author) {
+      quotes.push({ text, author });
+    }
+
+
+
+    this.setState({ showNewQuoteScreen: false, quotes });
+
+  };
+
+  render() {
+    let { index, quotes } = this.state;
+    const quote = quotes[index];
+    let nextIndex = index + 1;
+    if (nextIndex === quotes.length) nextIndex = 0;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonNew}>
+          <Button
+            title="Neu"
+            onPress={() => this.setState({ showNewQuoteScreen: true })}
+          />
+        </View>
+
+        <NewQuote
+          visible={this.state.showNewQuoteScreen}
+          onSave={this._addQuote}
+        />
+
+        <Quote text={quote.text} author={quote.author} />
+
+        <View style={styles.buttonNext}>
+          <Button
+            title="Nächstes Zitat"
+            onPress={() => this.setState({ index: nextIndex })} />
+        </View>
+      </View>
+    );
+  }
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +70,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
-});
+
+  buttonNext: {
+    position: 'absolute',
+    bottom: 30
+  },
+
+  buttonNew: {
+    position: 'absolute',
+    right: 30,
+    top: 50,
+  }
+
+}
+);
